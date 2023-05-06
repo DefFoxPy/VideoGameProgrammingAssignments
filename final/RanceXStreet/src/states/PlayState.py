6677ca774fe86498ec2f52aff62fbd52bf6cb14a
Mint <juanmpy05@gmail.com>
2023-05-06 10:40:46 -0400

    Update PlayState.py

--- final/RanceXStreet/src/states/PlayState.py
+++ final/RanceXStreet/src/states/PlayState.py
@@ -32,7 +32,7 @@ class PlayState(BaseState):
         self.time_car += 1
         self.score += 1
         
-        if self.time_car >= settings.GENERATE_CAR * random.randint(1, max(1, 1-self.score//100)):
+        if self.time_car >= settings.GENERATE_CAR * random.randint(1, max(1, 10-self.score//100)):
             aux_pos = random.randint(0,settings.NUM_VIAS-1)
             aux_skin = random.randint(0,settings.NUM_SKIN-1)
             old_posx_car = list()
